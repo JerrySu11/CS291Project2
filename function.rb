b3daa6cecfcd075d188205170f5f3579343ff6d1
Zihao Su <zihaosu@Zihaos-MacBook-Pro.local>
2022-10-05 22:37:08 -0700

    handle abnormal requests

--- function.rb
+++ function.rb
@@ -7,7 +7,38 @@ require 'pp'
 def main(event:, context:)
   # You shouldn't need to use context, but its fields are explained here:
   # https://docs.aws.amazon.com/lambda/latest/dg/ruby-context.html
-  response(body: event, status: 200)
+  if event["path"]!='/' && event["path"]!='/token'
+    return response(status:404)
+  end
+  if body["httpMethod"] == "GET"
+    return GETHandler(body)
+  elsif body["httpMethod"] == "POST"
+    return POSTHandler(body)
+  else:
+    return response(status:405)
+  end
+end
+
+def POSTHandler(body)
+  if body["path"]!='/'
+    return response(status:405) # path is '/token'
+  end
+  if body["headers"]["Content-Type"]!="application/json"
+    return response(status:415)
+  end
+  begin
+    content = JSON.parse(body["body"])
+    return response(body,200)
+  rescue JSON::ParserError => e
+    return response(status:422)
+  end
+end
+
+def GETHandler(body)
+  if body["path"]!="/token"
+    return response(status:405) # path is '/'
+  end
+  return response(body,200)
 end
 
 def response(body: nil, status: 200)
